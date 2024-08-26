dde7f75d4858b91bc5b15cdc1129336b092c13fb
ismailmesut <ismailmesutmujde@gmail.com>
2024-08-26 19:29:54 +0300

    updated: permission control

--- app/src/main/java/com/ismailmesutmujde/javaartbook/ArtActivity.java
+++ app/src/main/java/com/ismailmesutmujde/javaartbook/ArtActivity.java
@@ -1,9 +1,14 @@
 package com.ismailmesutmujde.javaartbook;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.ismailmesutmujde.javaartbook.databinding.ActivityArtBinding;
 
 
@@ -26,5 +31,22 @@ public class ArtActivity extends AppCompatActivity {
     }
     public void selectImage (View view) {
 
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                Snackbar.make(view, "Permission needed for gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // request permission
+                    }
+                }).show();
+
+            } else {
+                // request permission
+            }
+        } else {
+            // gallery
+
+        }
     }
 }
