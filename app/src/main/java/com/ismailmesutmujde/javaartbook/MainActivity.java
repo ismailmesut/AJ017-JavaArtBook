adfd224aff8877f5f678318c25e0b7e3781926a7
ismailmesut <ismailmesutmujde@gmail.com>
2024-08-27 00:57:09 +0300

    updated: get data

--- app/src/main/java/com/ismailmesutmujde/javaartbook/MainActivity.java
+++ app/src/main/java/com/ismailmesutmujde/javaartbook/MainActivity.java
@@ -1,6 +1,8 @@
 package com.ismailmesutmujde.javaartbook;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -12,10 +14,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.ismailmesutmujde.javaartbook.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    ArrayList<Art> artArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +29,29 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+        artArrayList = new ArrayList<>();
+        getData();
+    }
 
+    private void getData() {
+        try {
+            SQLiteDatabase sqLiteDatabase = this.openOrCreateDatabase("Arts", MODE_PRIVATE, null);
+            Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM arts", null);
+            int nameIx = cursor.getColumnIndex("artname");
+            int idIx = cursor.getColumnIndex("id");
+
+            while (cursor.moveToNext()) {
+                String name = cursor.getString(nameIx);
+                int id = cursor.getInt(idIx);
+                Art art = new Art(name, id);
+                artArrayList.add(art);
+            }
+
+            cursor.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
