715cd146d6d4bb090c3d42fcc11ebb40c6835195
ismailmesut <ismailmesutmujde@gmail.com>
2024-08-26 18:27:07 +0300

    updated: ui design

--- app/src/main/java/com/ismailmesutmujde/javaartbook/ArtActivity.java
+++ app/src/main/java/com/ismailmesutmujde/javaartbook/ArtActivity.java
@@ -1,16 +1,24 @@
 package com.ismailmesutmujde.javaartbook;
 
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class MainActivity extends AppCompatActivity {
+public class ArtActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_art);
 
     }
 
+    public void save (View view) {
+
+    }
+    public void selectImage (View view) {
+
+    }
 }
